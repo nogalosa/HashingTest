045a0efa8d4bba6cfe96b910757fc2abde6fc6e3
Nerijus Gabalis <ner147896523@gmail.com>
2019-09-26 10:39:01 +0300

    Implement NerHash object.

--- src/lt/nogalosa/hashingtest/Main.java
+++ src/lt/nogalosa/hashingtest/Main.java
@@ -2,7 +2,10 @@ package lt.nogalosa.hashingtest;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-	// write your code here
+
+
     }
 }
---
+++ src/lt/nogalosa/hashingtest/NerHash.java
@@ -0,0 +1,11 @@
+package lt.nogalosa.hashingtest;
+
+public class NerHash {
+
+    private String toHash;
+
+    public NerHash(String toHash) {
+        this.toHash = toHash;
+    }
+
+}
