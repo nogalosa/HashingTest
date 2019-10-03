aa09cf9e040d2ac07c4c4ce98d973659fe182b43
Nerijus Gabalis <ner147896523@gmail.com>
2019-10-03 10:53:37 +0300

    Added text generator.

--- src/lt/nogalosa/hashingtest/Main.java
+++ src/lt/nogalosa/hashingtest/Main.java
@@ -1,8 +1,8 @@
 package lt.nogalosa.hashingtest;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -17,9 +17,12 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Kokį hash norite hashuoti?");
+        System.out.println("Koki hash norite hashuoti?");
         System.out.println("1) Hashuoti \""+toHash+"\" (iš string)");
-        System.out.println("2) Hashuoti iš failo "+ (fileName!="" ? "("+fileName+")" : "(Negalima - failas nenurodytas)"));
+        System.out.println("2) Hashuoti is failo "+ (fileName!="" ? "("+fileName+")" : "(Negalima - failas nenurodytas)"));
+        System.out.println("Kita...");
+        System.out.println("3) Sugeneruoti a1000000.txt faila");
+
 
         String input = scanner.nextLine();
         int selection = 0;
@@ -37,6 +40,29 @@ public class Main {
         System.out.println("Pasirinkta " + selection + ".");
         System.out.println("");
 
+        if(selection == 3) {
+            System.out.println("Generuojama...");
+            ArrayList<String> generated = new ArrayList<>();
+            while(generated.size() < 1000000) {
+                String string = generateString() + ", " + generateString();
+                while(generated.contains(string))
+                    string = generateString() + ", " + generateString();
+                generated.add(string);
+            }
+            System.out.println("Irasoma...");
+            try {
+                BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+
+                for(String string : generated)
+                    writer.write(string + "\n");
+
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Baigta.");
+        }else
         if(selection == 2) {
             try {
                 File file = new File(fileName);
@@ -53,7 +79,7 @@ public class Main {
                     System.out.println(nerHash.hash());
                     time += nerHash.getTimeTaken()/1000;
                 }
-                System.out.println("Užtruko " + time + " μs (" + time/1000 + " ms)");
+                System.out.println("Uztruko " + time + " μs(mikro) (" + time/1000 + " ms)");
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
@@ -61,9 +87,27 @@ public class Main {
             NerHash nerHash = new NerHash(toHash);
             System.out.println(nerHash.hash());
             System.out.println("");
-            System.out.println("Užtruko " + nerHash.getTimeTaken()/1000 + " μs (" + nerHash.getTimeTaken()/1000/1000 + " ms)");
+            System.out.println("Užtruko " + nerHash.getTimeTaken()/1000 + " μs(mikro) (" + nerHash.getTimeTaken()/1000/1000 + " ms)");
         }
 
+    }
+
+    static Random random = new Random();
+
+    public static String generateString() {
+
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 5;
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+
+        return generatedString;
     }
 
 }
